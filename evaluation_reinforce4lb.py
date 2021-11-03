cae42b96f65712bdffba297f47af200f822f090e
Defeng <pandat8@gmail.com>
2021-11-03 02:24:11 -0400

    3a RL-acitve test t_limit_total=60s seed101

--- evaluation_reinforce4lb.py
+++ evaluation_reinforce4lb.py
@@ -19,7 +19,7 @@ rl_model_path = args.rl_model_path
 print(regression_model_path)
 print(rl_model_path)
 
-seed = 103
+seed = 104
 torch.manual_seed(seed)
 torch.cuda.manual_seed(seed)
 np.random.seed(seed)
