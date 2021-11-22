6f4e9c3eea10dd154528edb230e593837e0cc79f
Defeng <pandat8@gmail.com>
2021-11-22 16:10:07 -0500

    seed 100 dataset 4-5

--- compute_evaluation_results.py
+++ compute_evaluation_results.py
@@ -39,7 +39,7 @@ l = [3, 4, 1]
 # for lr in lr_list:
 #     print('learning rate = ', lr)
 #     print('epsilon = ', epsilon)
-for i in range(0, 5):
+for i in range(3, 5):
     instance_type = instancetypes[i]
     if instance_type == instancetypes[0]:
         lbconstraint_mode = 'asymmetric'
