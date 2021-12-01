7e663b044d6c218c1f12b8271955a06bf9535641
Defeng <pandat8@gmail.com>
2021-11-30 19:31:04 -0500

    upd

--- train_reinforce4lb_t_policy.py
+++ train_reinforce4lb_t_policy.py
@@ -15,6 +15,9 @@ parser.add_argument('--rl_k_policy_path', type = str, default='./result/saved_mo
 parser.add_argument('--seed', type=int, default=100, help='Radom seed') #50
 parser.add_argument('--t_reward_type', type=int, default=0, help='Reward signal for policy t, 0: reward_k, 1: reward_k + reward node time')
 parser.add_argument('--learning_rate', type=float, default=0.01, help='learning rate')
+parser.add_argument('--instance_type', type=int, default=0, help='Instance Type 0: sc, 1: mis, 2: ca, 3: gis, 4: miplib ')
+parser.add_argument('--instance_size', type=int, default=0, help='Instance Type 0: -small, 1: -large ')
+parser.add_argument('--incumbent_mode', type=int, default=0, help='Instance Type 0: -small, 1: -large ')
 args = parser.parse_args()
 
 # regression_model_path = args.regression_model_path
@@ -32,9 +35,12 @@ random.seed(seed)
 t_reward_type = t_reward_types[args.t_reward_type]
 print('t_reward_type: ', t_reward_type)
 # instance_type = instancetypes[1]
-instance_size = instancesizes[0]
+
+instance_type = instancetypes[args.instance_type]
+instance_size = instancesizes[args.instance_size]
+incumbent_mode = incumbent_modes[args.incumbent_mode]
+
 # incumbent_mode = 'firstsol'
-lbconstraint_mode = 'symmetric'
 samples_time_limit = 3
 
 total_time_limit = 600
@@ -53,32 +59,32 @@ epsilon = 0.0
 lr = args.learning_rate
 print('learning rate = ', lr)
 print('epsilon = ', epsilon)
-for i in range(4, 5):
-    instance_type = instancetypes[i]
-    if instance_type == instancetypes[0]:
-        lbconstraint_mode = 'asymmetric'
-    else:
-        lbconstraint_mode = 'symmetric'
-    for j in range(0, 1):
-        incumbent_mode = incumbent_modes[j]
+# for i in range(4, 5):
+#     instance_type = instancetypes[i]
+if instance_type == instancetypes[0]:
+    lbconstraint_mode = 'asymmetric'
+else:
+    lbconstraint_mode = 'symmetric'
+# for j in range(0, 1):
+#     incumbent_mode = incumbent_modes[j]
 
-        print(instance_type + instance_size)
-        print(incumbent_mode)
-        print(lbconstraint_mode)
+print(instance_type + instance_size)
+print(incumbent_mode)
+print(lbconstraint_mode)
 
-        reinforce_localbranch = RlLocalbranch(instance_type, instance_size, lbconstraint_mode, incumbent_mode, seed=seed)
+reinforce_localbranch = RlLocalbranch(instance_type, instance_size, lbconstraint_mode, incumbent_mode, seed=seed)
 
-        reinforce_localbranch.train_agent_policy_t(train_instance_size=instance_size,
-                                                   train_incumbent_mode=incumbent_mode,
-                                                   total_time_limit=total_time_limit,
-                                                   node_time_limit=node_time_limit,
-                                                   reset_k_at_2nditeration=reset_k_at_2nditeration,
-                                                   lr_t=lr,
-                                                   n_epochs=301,
-                                                   epsilon=epsilon,
-                                                   use_checkpoint=use_checkpoint,
-                                                   rl_k_policy_path=rl_k_policy_path,
-                                                   t_reward_type=t_reward_type)
+reinforce_localbranch.train_agent_policy_t(train_instance_size=instance_size,
+                                           train_incumbent_mode=incumbent_mode,
+                                           total_time_limit=total_time_limit,
+                                           node_time_limit=node_time_limit,
+                                           reset_k_at_2nditeration=reset_k_at_2nditeration,
+                                           lr_t=lr,
+                                           n_epochs=301,
+                                           epsilon=epsilon,
+                                           use_checkpoint=use_checkpoint,
+                                           rl_k_policy_path=rl_k_policy_path,
+                                           t_reward_type=t_reward_type)
 
         # reinforce_localbranch.evaluate_localbranching(evaluation_instance_size='-small', total_time_limit=total_time_limit, node_time_limit=node_time_limit, reset_k_at_2nditeration=reset_k_at_2nditeration)
 
