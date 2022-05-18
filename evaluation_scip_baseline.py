e353b4b89c1fbc54bd4fbe9481bbd44badd083f3
Defeng <pandat8@gmail.com>
2022-05-18 01:55:15 -0400

    evaluate Lb before node 2s, 60s, dataset1-3

--- evaluation_scip_baseline.py
+++ evaluation_scip_baseline.py
@@ -29,20 +29,20 @@ random.seed(seed)
 
 samples_time_limit = 3
 
-total_time_limit = 600 # 60 # 600# 60
+total_time_limit = 60 # 60 # 600# 60
 node_time_limit = 10 #10 # 60 # 5
 is_heuristic = True
 no_improve_iteration_limit = 10 # 3
 enable_solve_master_problem = True
 
 
-for i in range(3, 5):
+for i in range(0, 3):
     instance_type = instancetypes[i]
 
     for j in range(0, 2):
         incumbent_mode = incumbent_modes[j]
 
-        for k in range(0, 1):
+        for k in range(0, 2):
             instance_size = instancesizes[k]
 
             print(instance_type + instance_size)
