6231df8461ddcbbabff8c3328788ae81b0d5c12b
Defeng <pandat8@gmail.com>
2022-05-18 20:22:00 -0400

    evaluate Lb before node 2s, 60s,highpriority, dataset1-3

--- evaluation_scip_lb_rl.py
+++ evaluation_scip_lb_rl.py
@@ -31,7 +31,7 @@ random.seed(seed)
 
 samples_time_limit = 3
 
-total_time_limit = 600 # 60 # 600# 60
+total_time_limit = 60 # 60 # 600# 60
 node_time_limit = 2 #10 # 60 # 5
 is_heuristic = True
 no_improve_iteration_limit = 2 # 10 # 3
@@ -92,7 +92,7 @@ for i in range(0, 3):
                 evaluation_directory = evaluation_directory + 'heuristic_mode/'
 
             result_directory = evaluation_directory + 'lb-from-' + incumbent_mode + '-t_total' + str(
-                total_time_limit) + 's' + '-t_node' + str(node_time_limit) + 's' + instance_size + '_lb_k0_rl_beforenode/seed' + str(seed) + '/'
+                total_time_limit) + 's' + '-t_node' + str(node_time_limit) + 's' + instance_size + '_lb_k0_rl_beforenode_highpriority/seed' + str(seed) + '/'
             pathlib.Path(result_directory).mkdir(parents=True, exist_ok=True)
 
             scip_as_baseline = Execute_LB_RL(instance_directory,
