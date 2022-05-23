c5fb3964b0e8fbbc971acc972d5664986b7fb607
Defeng <pandat8@gmail.com>
2022-05-23 02:52:32 -0400

    upda

--- evaluation_scip_lb_regression.py
+++ evaluation_scip_lb_regression.py
@@ -33,7 +33,7 @@ samples_time_limit = 3
 total_time_limit = 60 # 60 # 600# 60
 node_time_limit = 1 #10 # 60 # 5
 is_heuristic = True
-no_improve_iteration_limit = 1 # 10 # 3
+no_improve_iteration_limit = 2 # 10 # 3
 enable_solve_master_problem = True
 
 regression_model_gnn = GNNPolicy()
