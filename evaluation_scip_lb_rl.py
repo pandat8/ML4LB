0ae9c9507ed2391e394a308f991d2a3ca73f398c
Defeng <pandat8@gmail.com>
2022-06-13 13:56:56 -0400

    evaluate multiple calls of LB as primal heuristic in SCIP, freq=100, when incumbent improves, low priority=-13000,before node, no_improvement_limit=2, dataset=5, noimprove=1

--- evaluation_scip_lb_rl.py
+++ evaluation_scip_lb_rl.py
@@ -31,10 +31,10 @@ random.seed(seed)
 
 samples_time_limit = 3
 
-total_time_limit = 60 # 60 # 600# 60
+total_time_limit = 600 # 60 # 600# 60
 node_time_limit = 2 #10 # 60 # 5
 is_heuristic = True
-no_improve_iteration_limit = 2 # 10 # 3
+no_improve_iteration_limit = 1 # 10 # 3
 enable_solve_master_problem = True
 
 lr = 0.01
@@ -65,7 +65,7 @@ rl_policy1 = rl_policy1.to(device)
 agent1 = AgentReinforce(rl_policy1, device, greedy, optim1, 0.0)
 
 
-for i in range(0, 3):
+for i in range(4, 5):
     instance_type = instancetypes[i]
     if instance_type == instancetypes[0]:
         lbconstraint_mode = 'asymmetric'
@@ -92,7 +92,7 @@ for i in range(0, 3):
                 evaluation_directory = evaluation_directory + 'heuristic_mode/'
 
             result_directory = evaluation_directory + 'lb-from-' + incumbent_mode + '-t_total' + str(
-                total_time_limit) + 's' + '-t_node' + str(node_time_limit) + 's' + instance_size + '_lb_k0_rl_beforenode_freq100/seed' + str(seed) + '/'
+                total_time_limit) + 's' + '-t_node' + str(node_time_limit) + 's' + instance_size + '_lb_k0_rl_beforenode_freq100_noimprove1/seed' + str(seed) + '/'
             pathlib.Path(result_directory).mkdir(parents=True, exist_ok=True)
 
             scip_as_baseline = Execute_LB_RL(instance_directory,
