4a85eaa7fbd213ec97da58d714ed29e425c58ff4
Defeng <pandat8@gmail.com>
2022-06-13 13:47:41 -0400

    evaluate multiple calls of LB as primal heuristic in SCIP, freq=100, when incumbent improves, low priority=-13000,before node, no_improvement_limit=2, dataset=1-3

--- evaluation_scip_lbbaseline.py
+++ evaluation_scip_lbbaseline.py
@@ -29,14 +29,14 @@ random.seed(seed)
 
 samples_time_limit = 3
 
-total_time_limit = 600 # 60 # 600# 60
+total_time_limit = 60 # 60 # 600# 60
 node_time_limit = 2 #10 # 60 # 5
 is_heuristic = True
 no_improve_iteration_limit = 2 # 10 # 3
 enable_solve_master_problem = True
 
 
-for i in range(3, 4):
+for i in range(0, 3):
     instance_type = instancetypes[i]
     if instance_type == instancetypes[0]:
         lbconstraint_mode = 'asymmetric'
