25e48f3dc9c3403960b4c9b399a2114c002ffd21
Defeng <pandat8@gmail.com>
2022-06-14 20:57:31 -0400

    upd

--- compute_evaluation_results_scip.py
+++ compute_evaluation_results_scip.py
@@ -75,10 +75,10 @@ for i in range(4, 5):
             # result directory of lns-lb, scip-lb-rl
 
             # result_directory_3 = evaluation_directory + 'lb-from-' + incumbent_mode + '-t_total' + str(
-            #     total_time_limit) + 's' + '-t_node' + str(node_time_limit) + 's' + instance_size + '_lb_k0_rl_beforenode_freq10/seed' + str(seed) + '/'
+            #     total_time_limit) + 's' + '-t_node' + str(node_time_limit) + 's' + instance_size + '_lb_k0_rl_beforenode_freq100/seed' + str(seed) + '/'
             result_directory_3 = evaluation_directory + 'lb-from-' + incumbent_mode + '-t_total' + str(
                 total_time_limit) + 's' + '-t_node' + str(
-                node_time_limit) + 's' + instance_size + '_lb_baseline_beforenode/seed' + str(seed) + '/'
+                node_time_limit) + 's' + instance_size + '_lb_baseline_beforenode_freq100_noimprove1/seed' + str(seed) + '/'
 
             # # result directory of lns-lblp
             # evaluation_directory = './result/generated_instances/' + instance_type + '/' + instance_size + '/' + incumbent_mode + '/' + 'lns' + '/'
@@ -92,11 +92,11 @@ for i in range(4, 5):
             # result directory of lns-lb-mcts, scip-lb-regression-rl
 
             # result_directory_5 = evaluation_directory + 'lb-from-' + incumbent_mode + '-t_total' + str(
-            #     total_time_limit) + 's' + '-t_node' + str(node_time_limit) + 's' + instance_size + '_lb_k0_regression_rl_beforenode_freq10/seed' + str(seed) + '/'
+            #     total_time_limit) + 's' + '-t_node' + str(node_time_limit) + 's' + instance_size + '_lb_k0_regression_rl_beforenode_freq100/seed' + str(seed) + '/'
 
             result_directory_5 = evaluation_directory + 'lb-from-' + incumbent_mode + '-t_total' + str(
                 total_time_limit) + 's' + '-t_node' + str(
-                node_time_limit) + 's' + instance_size + '_lb_k0_regression_beforenode/seed' + str(seed) + '/'
+                node_time_limit) + 's' + instance_size + '_lb_k0_regression_beforenode_freq100_noimprove1/seed' + str(seed) + '/'
 
             source_directory = './data/generated_instances/' + instance_type + '/' + instance_size + '/'
             instance_directory = source_directory + 'transformedmodel' + '/' + 'test/'
