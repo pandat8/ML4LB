123c4fd97a4ab8864da49429f9f04f1c5e876b09
Defeng <pandat8@gmail.com>
2022-06-14 21:10:59 -0400

    plot dataset 1-3, freq=100

--- compute_evaluation_results_scip.py
+++ compute_evaluation_results_scip.py
@@ -30,11 +30,11 @@ random.seed(seed)
 
 samples_time_limit = 3
 
-total_time_limit = 600
+total_time_limit = 60
 node_time_limit = 2
 is_heuristic = True
 
-for i in range(3, 4):
+for i in range(0, 3):
     instance_type = instancetypes[i]
     if instance_type == instancetypes[0]:
         lbconstraint_mode = 'asymmetric'
