bb62b3bd3c165e9a299e3c7c8dfe9d60f8007891
Defeng <pandat8@gmail.com>
2022-06-19 02:02:01 -0400

    train reinforce t-policy t-reward: when case 4,t-reward=-1

--- train_reinforce4lb_t_policy.py
+++ train_reinforce4lb_t_policy.py
@@ -14,7 +14,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--rl_k_policy_path', type = str, default='./result/saved_models/rl/reinforce/setcovering/checkpoint_trained_reward3_simplepolicy_rl4lb_reinforce_trainset_setcovering-small_lr0.01_epochs7.pth')
 parser.add_argument('--seed', type=int, default=100, help='Random seed') #50
 parser.add_argument('--t_reward_type', type=int, default=0, help='Reward signal for policy t, 0: reward_k, 1: reward_k + reward node time')
-parser.add_argument('--learning_rate', type=float, default=0.1, help='learning rate')
+parser.add_argument('--learning_rate', type=float, default=0.0001, help='learning rate')
 parser.add_argument('--instance_type', type=int, default=0, help='Instance Type 0: sc, 1: mis, 2: ca, 3: gis, 4: miplib ')
 parser.add_argument('--instance_size', type=int, default=0, help='Instance Type 0: -small, 1: -large ')
 parser.add_argument('--incumbent_mode', type=int, default=0, help='Instance Type 0: -small, 1: -large ')
