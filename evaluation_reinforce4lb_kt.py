005f506547ae2a6291bde80c42c010de0b56db53
Defeng <pandat8@gmail.com>
2022-07-04 03:56:51 -0400

    upd

--- evaluation_reinforce4lb_kt.py
+++ evaluation_reinforce4lb_kt.py
@@ -66,7 +66,7 @@ l = [3, 4, 1]
 # for lr in lr_list:
 #     print('learning rate = ', lr)
 #     print('epsilon = ', epsilon)
-for i in range(3, 4):
+for i in range(4, 5):
     instance_type = instancetypes[i]
     if instance_type == instancetypes[0]:
         lbconstraint_mode = 'asymmetric'
