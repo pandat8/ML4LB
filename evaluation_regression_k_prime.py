031a919578d378f677a201b254ee07c421caa6e6
Defeng <pandat8@gmail.com>
2022-07-06 20:07:18 -0400

    up

--- evaluation_regression_k_prime.py
+++ evaluation_regression_k_prime.py
@@ -38,7 +38,7 @@ for k in range(0, 2):
             lbconstraint_mode = 'asymmetric'
         else:
             lbconstraint_mode = 'symmetric'
-        for j in range(1, 2):
+        for j in range(0, 1):
             incumbent_mode = incumbent_modes[j]
 
             print(instance_type + test_instance_size)
