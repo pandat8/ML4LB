2c8291465b0e572b9d4da60561a9664fac3a3b47
Defeng <pandat8@gmail.com>
2022-07-13 16:25:04 -0400

    upd

--- evaluation_reinforce4lb_kt.py
+++ evaluation_reinforce4lb_kt.py
@@ -80,7 +80,7 @@ for k in range(0, 2):
         else:
             lbconstraint_mode = 'symmetric'
 
-        for j in range(0, 1):
+        for j in range(1, 2):
             incumbent_mode = incumbent_modes[j]
 
             print(instance_type + test_instance_size)
