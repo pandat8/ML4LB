594d5b9aee8983b1554bcd5dccc740d332fb0771
Defeng <pandat8@gmail.com>
2023-02-19 19:54:10 -0500

    evaluation reinforce only k, 600s, dataset5

--- evaluation_reinforce4lb.py
+++ evaluation_reinforce4lb.py
@@ -58,7 +58,7 @@ l = [3, 4, 1]
 for k in range(0, 2):
     test_instance_size = instancesizes[k]
 
-    for i in range(3, 5):
+    for i in range(4, 5):
         instance_type = instancetypes[i]
         if instance_type == instancetypes[0]:
             lbconstraint_mode = 'asymmetric'
