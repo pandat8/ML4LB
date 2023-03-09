0071e529a839278810a70323a2d1b427b0e053cf
Defeng <pandat8@gmail.com>
2023-03-08 22:34:35 -0500

    upd

--- evaluation_scip_lb_regression_rl.py
+++ evaluation_scip_lb_regression_rl.py
@@ -24,7 +24,7 @@ rl_model_path = args.rl_model_path
 print(regression_model_path)
 print(rl_model_path)
 
-seed = 120 # 1
+seed = 100 # 1
 torch.manual_seed(seed)
 torch.cuda.manual_seed(seed)
 np.random.seed(seed)
